397962fddc0619531db85025feeddcd8b8c3a74a
Mayra Amaral <mayracunha18@gmail.com>
2024-09-17 17:26:19 -0300

    feat: initial structure

---
+++ src/test/java/playground/grpc/GrpcPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package playground.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
